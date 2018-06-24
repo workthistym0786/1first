deb09886fb39a63886bd1a82b0f94676672a11c0
workthistym0786 <40546061+workthistym0786@users.noreply.github.com>
2018-06-24 18:47:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.amazon.com/Goodthreads-Slim-Fit-Long-Sleeve-Herringbone-Grey/dp/B072VHNZ81/ref=sr_1_18?s=apparel&ie=UTF8&qid=1527231094&sr=1-18&nodeID=7141123011&psd=1&keywords=shirt");
 die();
 ?>
